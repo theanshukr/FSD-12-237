412555e81adbc0cb8fd461fbc34d989327624f08
Theanshukr <imanshu2702@gmail.com>
2026-07-24 14:37:06 +0530

    ev

---
+++ lab/event.js
@@ -0,0 +1,16 @@
+
+const login=(name)=>{
+    console.log(`Welcome ${name} logged in successfully`);
+}
+const strat=()=>{
+    console.log("Starting the application");
+}
+const working=(name)=>{
+    console.log(`${name} add items to cart`);
+}
+const checkout=(name)=>{
+    console.log(`${name} logged out`);
+
+}
+
+login("Alec");
--- lab/fun.js
+++ lab/fun.js
@@ -1,6 +1,8 @@
 //wap to take any digit 0-9 and return in words
+//node js spring boot faast api django php 
 const word = (a) => {
     const words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    return words[a];
+    return words[a]
 }
-console.log(word(0)); 
+console.log(word(0));
+console.log(word(5)); 
